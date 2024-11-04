9f188c8aa3311fb37ed2bf8ae3811cb6aec66daa
MedAmineHm <mohamedamine.hammami@esprit.tn>
2024-11-04 15:05:39 +0100

    dock

--- backend/src/schemas/User.schema.ts
+++ backend/src/schemas/User.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { Types } from 'mongoose';
 
 @Schema()
